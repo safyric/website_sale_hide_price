98e42ae7318ece6cd7268c9329df1d6fa87fc5b6
jchanme <39323414+jchanme@users.noreply.github.com>
2018-12-03 13:55:54 +0800

    Create website.py

---
+++ models/website.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+from odoo.http import request
+
+class Website(models.Model):
+    _inherit = "website"
+
+    website_show_price = fields.Boolean(compute='_compute_website_show_price')
+
+    def _compute_website_show_price(self):
+        for rec in self:
+            rec.website_show_price = (request.env.user.partner_id.website_show_price)
