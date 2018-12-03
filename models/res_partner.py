553bdf3c86d9fd369b193c14b718a8396fe00cb7
jchanme <39323414+jchanme@users.noreply.github.com>
2018-12-03 14:02:27 +0800

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-
+    
     website_show_price = fields.Boolean(string='Show prices on website', default=False,)
